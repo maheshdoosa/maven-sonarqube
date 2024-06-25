f3c879e418a4370754f6ba3e8adc31c529900206
maheshdoosa <86100265+maheshdoosa@users.noreply.github.com>
2024-06-25 21:48:26 +0530

    Add files via upload

---
+++ src/main/java/com/maheshdoosa/first_demo_maheshdoosa/App.java
@@ -0,0 +1,13 @@
+package com.maheshdoosa.first_demo_maheshdoosa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
